193f14119b4feb963c56ae4d4437509588308668
Pushkar N Kulkarni <pushkar.rvce@gmail.com>
2016-10-15 11:57:48 +0530

    Added a monad example

--- module5/Functor.swift
+++ module5/Functor.swift
@@ -1,35 +1,15 @@
-//Optional is a functor
-class Customer {
-    let name: String
-    let age: Int
-
-    public init? (_ name: String, _ age: Int) {
-        guard age < 115 else { return nil }
-        self.name = name
-        self.age = age
-    }
+//Optionals and Arrays are functors
+func stringLength(_ str: String?) -> Int? {
+    return str.map { $0.characters.count }
 }
 
-let customer = Customer("Harshad", 20)
-
-//not cool
-if let c = customer {
-    print(c.name)
+func stringLengths(_ strs: [String]) -> [Int] {
+    return strs.map { $0.characters.count }
 }
 
-//cool
-let name = customer.map { $0.name }
-print(name)
 
-let customer1 = Customer("Rustom", 120)
-let name1 = customer1.map { $0.name }
-print(name1)
+print(stringLength(nil))
+print(stringLength("fresh"))
 
-//find the length of the name
-let len0 = customer.map { $0.name }
-                   .map { $0.characters.count }
-print(len0)
-
-let len1 = customer1.map { $0.name }
-                    .map { $0.characters.count }
-print(len1)
+print(stringLengths([]))
+print(stringLengths(["functionalConf", "2016"]))
